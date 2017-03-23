7d75a416c91d3cddfa7ffd19db6c31adab6733ae
Titouan <titouanfreville@gmail.com>
2017-03-23 17:50:57 +0100

    Some TPS 4AIT prolog

--- 4AIT/Prologue/tp4.pl
+++ 4AIT/Prologue/tp4.pl
@@ -1,37 +1,135 @@
-%datas:
-%%  asserta(ferrari:-italienne)
-%%  asserta(lamborgini:-italienne)
-%%  asserta(ford:- americaine)
-%%  asserta(peugo:- francaise)
-%%  asserta(talbot:- francaise)
-%%  asserta(chrysler:- americaine)
-%%  asserta(italienne:- red, prix(10000, 20000))
-%%  asserta(francaise:- bleu, prix(5000, 15000))
-%%  asserta(americaine:- blanche, prix(10000, 15000))
+% <><><><><><><><><><><><><><><><> 4.1 CARS <><><><><><><><><><><><><><><><> %
 %clauses:
-  ferrari(ferrari).
-  lamborgini(lamborgini).
-  ford(ford).
-  peugo(peugo).
-  talbot(talbot).
-  chrysler(chrysler).
-  price1(10000, 20000).
-  price2(5000, 15000).
-  price3(7500, 12500).
-%foncteur:
-  %% LOGIC
-  isIt(X):- ferrari(X); lamborgini(X).
-  isUs(X):- ford(X); chrysler(X).
-  isFr(X):- peugo(X); talbot(X).
-  isRed(X):- isIt(X); isPrice1(X).
-  isBlue(X):- isFr(X); isPrice2(X).
-  isWhite(X):- isUs(X); isPrice3(X).
-  isPrice1(X):- isIt(X); isRed(X).
-  isPrice2(X):- isFr(X); isBlue(X).
-  isPrice3(X):- isUs(X); isWhite(X).
-  %% GETTERS
-  whatPrice(X, Y, Z):- (isPrice1(X), price1(Y, Z)); (isPrice2(X), price2(Y, Z)); (isPrice3(X) , price3(Y, Z)).
-  %% MATHS
-  cheapCars(X):- isPrice2(X); isPrice3(X).
-  expensiveCars(X):- isPrice1(X); isPrice2(X); isPrice3(X).
-  medCar(X):- isPrice1(X); isPrice2(X); isPrice3(X).
+  % Ferraris models
+  ferrari(f12).
+  ferrari(488).
+  ferrari(california).
+  % Lamborgini models
+  lamborgini(aventador).
+  lamborgini(huracan).
+  lamborgini(aventador).
+  % Ford models
+  ford(fiesta).
+  ford(focus).
+  ford(mustang).
+  ford(mondeo).
+  ford(ecosport).
+  % Peugo models
+  peugo(405).
+  peugo(408).
+  peugo(407).
+  % Talbot models
+  talbot(simca).
+  talbot(horizon).
+  talbot(samba).
+  talbot(tagora).
+  % Chyrsler models
+  chrysler(airflow).
+  chrysler(alpine).
+  chrysler(avenger).
+  % Price range
+  priceRange1(10000).
+  priceRange1(20000).
+  priceRange2(5000).
+  priceRange2(15000).
+  priceRange3(7500).
+  priceRange3(12500).
+
+%foncteurs:
+  % Nat. 
+  it(X):-ferrari(X).
+  it(X):-lamborgini(X).
+  us(X):-ford(X).
+  us(X):-chrysler(X).
+  fr(X):-peugo(X).
+  fr(X):-talbot(X).
+  % Couleur
+  red(X):-it(X).
+  blue(X):-fr(X).
+  white(X):-us(X).
+  % Car prices
+  % I used the top logical condition to check price range. 
+  price(X,Res, List):-red(X), findall(Res, priceRange1(Res), List).
+  price(X,Res, List):-blue(X), findall(Res, priceRange2(Res), List).
+  price(X,Res, List):-white(X), findall(Res, priceRange1(Res), List).
+  % Get car into some price range (let increment prices by 500 each time for this exemple.
+  %% Overs solution will come ... 
+  cheapcars(X,Res):-price(X,Res,[5000|_]).
+  cheapcars(X,Res):-price(X,Res,[7500|_]).
+  medcars(X,Res):-price(X,Res,[10000|_]).
+  medcars(X,Res):-price(X,Res,[_,12500]).
+  bigcars(X,Res):-price(X,Res,[_,20000]).
+  bigcars(X,Res):-price(X,Res,[_,15000]).
+  bigcars(X,Res):-price(X,Res,[_,12500]).
+% Exec in REPL.
+% %but
+%   red(X).
+%   ford(X).
+%   cheapcars(X,Res).
+%   bigcars(X,Res).
+%   medcars(X,Res).
+% <><><><><><><><><><><><><><><><> <><><><> <><><><><><><><><><><><><><><><> %
+% <><><><><><><><><><><><><><><><> 4.2 TREE <><><><><><><><><><><><><><><><> %
+%clauses
+  homme(jean).
+  homme(richard).
+  homme(nicolas).
+  homme(francois).
+  homme(bernard).
+  homme(paul).
+  homme(thomas).
+  homme(christophe).
+  homme(hugo).
+  femme(anna).
+  femme(helene).
+  femme(marthe).
+  femme(annette).
+  femme(clara).
+  femme(lise).
+  femme(nicole).
+  femme(eve).
+  femme(ema).
+  femme(marie).
+  parent(annette,anna).
+  parent(richard,jean).
+  parent(richard,helene).
+  parent(nicole,helene).
+  parent(nicole,jean).
+  parent(francoise,paul).
+  parent(francois,marthe).
+  parent(bernard,paul).
+  parent(bernard,marthe).
+  parent(clara,richard).
+  parent(clara,annette).
+  parent(nicolas,richard).
+  parent(nicolas,annette).
+  parent(thomas,nicolas).
+  parent(thomas,francois).
+  parent(christophe,nicole).
+  parent(christophe,francois).
+  parent(eve,nicole).
+  parent(eve,francois).
+  parent(lise,nicolas).
+  parent(marie,nicolas).
+  parent(ema,thomas).
+  parent(hugo,thomas).
+
+%foncteurs
+  pere(X, Pere):- parent(X, Pere), homme(Pere).
+  allpere(Peres):- findall(Pere, pere(_,Pere), Peres).
+  mere(X, Mere):- parent(X, Mere), femme(Mere).
+  allmere(Meres):- findall(Mere, mere(_,Mere), Meres).
+  grandparent(X,GrandParent):- parent(X,Y), parent(Y,GrandParent).
+  childrens(Childs):- findall(Child, parent(Child,_), Childs).
+  cousin(X, Cousin):- grandparent(X, Z), grandparent(Cousin, Z).
+  allcousin(Cousins):- findall(Cousin, cousin(_,Cousin),Cousins).
+% %but
+%   pere(nicolas, Pere).
+%   allpere(Pere).
+%   allmere(Meres).
+%   childrens(Chils).
+%   allcousin(Cousins).
+% <><><><><><><><><><><><><><><><> <><><><> <><><><><><><><><><><><><><><><> %
+% <><><><><><><><><><><><><><><><> 4.3 PLAY <><><><><><><><><><><><><><><><> %
+
+% <><><><><><><><><><><><><><><><> <><><><> <><><><><><><><><><><><><><><><> %
