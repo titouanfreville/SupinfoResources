a308f77e32fa45b4b5d378fe0f548404868dfc1d
Titouan <titouanfreville@gmail.com>
2017-04-07 14:19:25 +0200

    Cleaned folder

--- 2CMP/lex_yacc/tp6/test.sh
+++ 2CMP/lex_yacc/tp6/test.sh
@@ -2,23 +2,23 @@
 
 CRYPT_KEY=10
 
-file="$1"
-file2="${2:-$file}"
-file3="${3:-$file2}"
-file4="${4:-out.from.lex}"
-file5="${5:-$file3}"
-file6="${6:-$file3}"
+file="tests/$1"
+file2="tests/${2:-$file}"
+file3="tests/${3:-$file2}"
+file4="tests/${4:-out.from.lex}"
+file5="tests/${5:-$file3}"
+file6="tests/${6:-$file3}"
 
 if [[ -z "$file" ]]
 then
-    echo -e "Usage: \t./test.sh file_for_tp_6_1 [file_for_tp_6_2] [file_in_for_tp_6_3] [file_out_for_tp_6_3][file_for_tp_5][file_for_polish_checker]"
+    echo -e "Usage: \t./compiled/test.sh file_for_tp_6_1 [file_for_tp_6_2] [file_in_for_tp_6_3] [file_out_for_tp_6_3][file_for_tp_5][file_for_polish_checker]"
     exit 1
 fi
 
 echo -e "Running copie.\n\nCopie should write the file you provided as argument in the stdout (provided file: <$file>)"
 echo
 echo "-------- Pgm output ----------"
-./copie_from_lex "$file"
+./compiled/copie_from_lex "$file"
 echo "------------------------------"
 echo
 
@@ -27,7 +27,7 @@ echo
 echo -e "\n\nRunning compt voyelle.\n\nShould show a message with the number of voyelle in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_voyelle "$file"
+./compiled/compt_voyelle "$file"
 echo "------------------------------"
 echo
 [[ $? -eq 0 ]] && echo "Looks good ?" || { echo "Execution fail"; exit 1; }
@@ -35,7 +35,7 @@ echo
 echo -e "\n\nRunning compt consonne.\n\nShould show a message with the number of consonne in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_consonne "$file"
+./compiled/compt_consonne "$file"
 echo "------------------------------"
 echo
 [[ $? -eq 0 ]] && echo "Looks good ?" || { echo "Execution fail"; exit 1; }
@@ -43,7 +43,7 @@ echo
 echo -e "\n\nRunning compt mot.\n\nShould show a message with the number of mot in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_mot "$file"
+./compiled/compt_mot "$file"
 echo "------------------------------"
 echo
 
@@ -52,7 +52,7 @@ echo
 echo -e "\n\nRunning compt linebreak.\n\nShould show a message with the number of linebreak in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_linebreak "$file"
+./compiled/compt_linebreak "$file"
 echo "------------------------------"
 echo
 
@@ -61,7 +61,7 @@ echo
 echo -e "\n\nRunning compt allchars.\n\nShould show a message with the number of allchars in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_allchars "$file"
+./compiled/compt_allchars "$file"
 echo "------------------------------"
 echo
 
@@ -70,7 +70,7 @@ echo
 echo -e "\n\nRunning compt chars_notarbreak.\n\nShould show a message with the number of chars_notarbreak in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_chars_notarbreak "$file"
+./compiled/compt_chars_notarbreak "$file"
 echo "------------------------------"
 echo
 
@@ -79,7 +79,7 @@ echo
 echo -e "\n\nRunning compt class.\n\nShould show a message with the number of class in provided file (<$file>) "
 echo
 echo "-------- Pgm output ----------"
-./compt_class "$file"
+./compiled/compt_class "$file"
 echo "------------------------------"
 echo
 
@@ -93,7 +93,7 @@ echo
 echo "------------------------------"
 echo
 echo "-------- Pgm output ----------"
-./filt_sl_comment "$file2"
+./compiled/filt_sl_comment "$file2"
 echo "------------------------------"
 echo
 
@@ -107,7 +107,7 @@ echo
 echo "------------------------------"
 echo
 echo "-------- Pgm output ----------"
-./filt_ml_comment "$file2"
+./compiled/filt_ml_comment "$file2"
 echo "------------------------------"
 echo
 
@@ -121,7 +121,7 @@ echo
 echo "------------------------------"
 echo
 echo "-------- Pgm output ----------"
-./filt_chars "$file2"
+./compiled/filt_chars "$file2"
 echo "------------------------------"
 echo
 
@@ -131,7 +131,7 @@ echo -e "Running copie.\n\nCopie <$file3> in <$file4>)"
 echo
 echo > "$file4"
 echo "-------- Pgm output ----------"
-./copie_files "$file3" "$file4"
+./compiled/copie_files "$file3" "$file4"
 echo "cat $file4"
 cat "$file4"
 echo "------------------------------"
@@ -143,7 +143,7 @@ echo -e "Running crypt.\n\Encrypting <$file3> in <$file3.crypt>)"
 echo
 echo > "$file3.crypt"
 echo "-------- Pgm output ----------"
-./crypt "$file3" "$file3.crypt" $CRYPT_KEY
+./compiled/crypt "$file3" "$file3.crypt" $CRYPT_KEY
 echo "cat $file3.crypt"
 cat "$file3.crypt"
 echo "------------------------------"
@@ -155,7 +155,7 @@ echo -e "Running crypt.\n\Decrypting <$file3.crypt> in <$file3.decrypt>)"
 echo
 echo > "$file3.decrypt"
 echo "-------- Pgm output ----------"
-./decrypt "$file3.crypt" "$file3.decrypt" $CRYPT_KEY
+./compiled/decrypt "$file3.crypt" "$file3.decrypt" $CRYPT_KEY
 echo "cat $file3.decrypt"
 cat "$file3.decrypt"
 echo "------------------------------"
@@ -167,7 +167,7 @@ echo -e "Running crypt.\n\Decrypting <$file3.crypt> in <$file3.decrypt>)"
 echo
 echo > "$file3.decrypt"
 echo "-------- Pgm output ----------"
-./decrypt "$file3.crypt" "$file3.decrypt" $CRYPT_KEY
+./compiled/decrypt "$file3.crypt" "$file3.decrypt" $CRYPT_KEY
 echo "cat $file3.decrypt"
 cat "$file3.decrypt"
 echo "------------------------------"
@@ -178,7 +178,7 @@ echo
 echo -e "Running print nb.\n\nPrinting numbers from user input"
 echo
 echo "-------- Pgm output ----------"
-./print_nb
+./compiled/print_nb
 echo "------------------------------"
 echo
 
@@ -187,7 +187,7 @@ echo
 echo -e "Running calc.\n\nCalc from user input"
 echo
 echo "-------- Pgm output ----------"
-./calc
+./compiled/calc
 echo "------------------------------"
 echo
 
@@ -196,7 +196,7 @@ echo
 echo -e "Running pgm analysor.\n\nPrint operator, ... from <$file5>"
 echo
 echo "-------- Pgm output ----------"
-./pgm_analysor "$file5"
+./compiled/pgm_analysor "$file5"
 echo "------------------------------"
 echo
 
@@ -205,7 +205,7 @@ echo
 echo -e "Running polish syntax analysor - no automata.\n\nComment correctness of expression from <$file6>"
 echo
 echo "-------- Pgm output ----------"
-./polish_syntax_checker "$file6"
+./compiled/polish_syntax_checker "$file6"
 echo "------------------------------"
 echo
 
@@ -214,7 +214,7 @@ echo
 echo -e "Running polish syntax analysor - with automata.\n\nComment correctness of expression from <$file6>"
 echo
 echo "-------- Pgm output ----------"
-./polish_syntax_checker_automate "$file6"
+./compiled/polish_syntax_checker_automate "$file6"
 echo "------------------------------"
 echo
 
